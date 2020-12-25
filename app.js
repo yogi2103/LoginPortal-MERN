31eb5b920373cfb9d642ed4b903609acb8f1b3ca
Yogesh Kumar <yogesh.baghel86@gmail.com>
2020-12-26 00:46:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path=require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 const cookieParser = require('cookie-parser');
@@ -14,6 +15,10 @@ mongoose.connect('mongodb+srv://yogi:21032103@cluster0.nmyxz.mongodb.net/mernaut
 if (process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'));
 }
+
+app.get('*',(req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 const userRouter = require('./routes/User');
 app.use('/user',userRouter);
